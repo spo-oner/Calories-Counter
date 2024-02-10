5b8a6d65254855f2c523e4865e6d39ae22e126ae
Vladislav Chernyavskii <44117751+spo-oner@users.noreply.github.com>
2024-02-10 13:13:54 +0300

    Update script.js
    rus

--- script.js
+++ script.js
@@ -55,7 +55,7 @@ function calculateCalories(e) {
 
   const consumedCalories = breakfastCalories + lunchCalories + dinnerCalories + snacksCalories;
   const remainingCalories = budgetCalories - consumedCalories + exerciseCalories;
-  const surplusOrDeficit = remainingCalories < 0 ? 'Surplus' : 'Deficit';
+  const surplusOrDeficit = remainingCalories < 0 ? 'В избытке' : 'Недостаточно';
   output.innerHTML = `
   <span class="${surplusOrDeficit.toLowerCase()}">${Math.abs(remainingCalories)} Калории ${surplusOrDeficit}</span>
   <hr>
